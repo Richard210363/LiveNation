0ba834cd38f06ce1c9ee9892b71ff5f46c2339e7
Richard Byrne <richard.login@gmail.com>
2022-03-23 21:49:51 +0000

    Update NumberRangeProcessingController.cs
    Added guard clause

--- LiveNation.WebAPI/Controllers/NumberRangeProcessingController.cs
+++ LiveNation.WebAPI/Controllers/NumberRangeProcessingController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using LiveNation.WebAPI.DataProcessing;
 using LiveNation.WebAPI.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace LiveNation.WebAPI.Controllers
 {
@@ -10,15 +11,30 @@ namespace LiveNation.WebAPI.Controllers
     {
 
         private readonly INumberRangeProcessor _numberRangeProcessor;
-        
+        private readonly IMemoryCache _memoryCache;
+
+
         public NumberRangeProcessingController(INumberRangeProcessor numberRangeProcessor)
+        //public NumberRangeProcessingController(INumberRangeProcessor numberRangeProcessor, IMemoryCache memoryCache)
         {
+
+
             this._numberRangeProcessor = numberRangeProcessor;
+            //this._memoryCache = memoryCache;
         }
 
         [HttpGet]
         public NumberRangeProcessingResult Get(int startRange, int endRange)
         {
+            if(endRange < startRange)
+            {
+                return new NumberRangeProcessingResult
+                {
+                    Result = "Please make endRange larger than or equal to startRange."
+                };
+
+            }
+
             return _numberRangeProcessor.ProcessNumberRange(startRange, endRange);
         }
     }
